77d9f809c5058a1d522ba3dbce8d14f07503a26f
Daniel Soares <daniel.soares.t0126446@edu.atec.pt>
2025-02-20 09:57:36 +0000

    Elaboração de Exercício 1

--- UFCD 10789/Exercicio1/Exercicio1.c
+++ UFCD 10789/Exercicio1/Exercicio1.c
@@ -2,7 +2,11 @@
 
 int main(){
 
-    printf("Hello world!");
+    int Num=0;
+    for(int i=0; i<10; i++){
+        Num+=2;
+        printf("Num = %d, i = %d\n", Num, i);
+        printf("aqui1FOR\n\n");
+    }
     return 0;
-
 }

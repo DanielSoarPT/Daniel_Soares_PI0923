04ec9a5bce79a50a55f4b1dc8f50d704cfa91ea4
Daniel Soares <daniel.soares.t0126446@edu.atec.pt>
2025-02-25 12:41:48 +0000

    Exercício 5

---
+++ UFCD 10789/Exercicio5/Exercicio5.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+int main(){
+
+    // Ler 3 valores INTEIROS para as variáveis Num1, Num2, Num3. Apresentar os valores dispostos em ordem crescente e decrescente.
+
+    int num1, num2, num3, temp;
+
+    printf("Insira o valor para o num1: ");
+    scanf("%d", &num1);
+    printf("Insira o valor para o num2: ");
+    scanf("%d", &num2);
+    printf("Insira o valor para o num3: ");
+    scanf("%d", &num3);
+    printf("\n");
+
+    printf("Ordem crescente: ");
+
+    if(num1>num2){
+        if(num2>num3){
+            printf("%d %d %d", num3, num2, num1);
+        } else{
+            if(num1>num3){
+                printf("%d %d %d", num2, num3, num1);
+            }
+            else{
+                    printf("%d %d %d", num2, num1, num3);
+            }
+        }
+    } else{
+        if(num2>num3){
+            if(num1>num3){
+                printf("%d %d %d", num3, num1, num2);
+            } else{
+                printf("%d %d %d", num1, num3, num2);
+            }
+        } else{
+            printf("%d %d %d", num1, num2, num3);
+        }
+    }
+
+    printf("\nOrdem decrescente: ");
+
+    if(num1>num2){
+        if(num2>num3){
+            printf("%d %d %d", num1, num2, num3);
+        } else{
+            if(num1>num3){
+                printf("%d %d %d", num1, num3, num2);
+            }
+            else{
+                    printf("%d %d %d", num3, num1, num2);
+            }
+        }
+    } else{
+        if(num2>num3){
+            if(num1>num3){
+                printf("%d %d %d", num2, num1, num3);
+            } else{
+                printf("%d %d %d", num2, num3, num1);
+            }
+        } else{
+            printf("%d %d %d", num3, num2, num1);
+        }
+    }
+
+    return 0;
+
+}

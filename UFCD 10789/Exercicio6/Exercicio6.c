371d0d2361d7be1759aca4d08fb8771134ff8636
Daniel Soares <daniel.soares.t0126446@edu.atec.pt>
2025-02-25 12:59:23 +0000

    Exercício 6

---
+++ UFCD 10789/Exercicio6/Exercicio6.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(){
+
+    // Uma loja oferece para os seus clientes, um determinado desconto de acordo com o valor da compra efetuada.
+    // O desconto � de 10%, se o valor da compra for at� 200.00�, 15% se for maior que  200� e menor ou igual a  500,00E e 20% se for acima de 500,00�.
+    // Crie um algoritmo que leia o nome do cliente e o valor da compra. Mostre ao final o nome do cliente, o valor da compra,
+    // o percentual de desconto e o seu valor e valor total a pagar deste cliente. (s� fazer duas Contas)
+
+    char nome[50];
+    float percentagem, total, compra;
+
+    printf("Insira o seu nome: ");
+    scanf("%s", nome);
+
+    printf("Insira o valor da sua compra: ");
+    scanf("%f", &compra);
+
+    if(compra<=200){
+        percentagem = 0.1;
+        total = compra;
+        total -= compra * percentagem;
+    } else{
+        if(compra<=500){
+            percentagem = 0.15;
+            total = compra;
+            total -= compra * percentagem;
+        } else{
+            percentagem = 0.2;
+            total = compra;
+            total -= compra * percentagem;
+        }
+    }
+
+    printf("\nNome do comprador: %s", nome);
+    printf("\nValor da compra: %.2f", compra);
+    printf("\nDesconto: %.0f porcento", percentagem * 100);
+    printf("\nTotal: %.2f", total);
+
+    return 0;
+
+}

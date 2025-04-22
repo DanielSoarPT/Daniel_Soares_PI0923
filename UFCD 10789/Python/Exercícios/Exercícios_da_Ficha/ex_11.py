a2b296e0cdcca311c7a9abe7c5d247aeb25308de
Daniel Soares <daniel.soares.t0126446@edu.atec.pt>
2025-04-22 13:34:04 +0100

    Novo Exercício de Python

---
+++ UFCD 10789/Python/Exercícios/Exercícios_da_Ficha/ex_11.py
@@ -0,0 +1,22 @@
+# Elabore um ciclo for para produzir o seguinte output.
+#	1
+#	22
+#	333
+#	4444
+#	55555
+
+while True:
+    try:
+        print("Insira um numero")
+        num = int(input(">> "))
+        print("")
+    except ValueError:
+        print("\nInsira um numero valido\n")
+        continue
+
+    for i in range(1,num+1):
+        for j in range(1, i+1):
+            print(i, end="")
+        print()
+    
+    break
